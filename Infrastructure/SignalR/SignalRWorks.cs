90ac68ab3a93131197dc839f24bdf4b9527ab5e9
Unknown <imp908@yandex.ru>
2019-06-14 01:55:20 +0300

    Auto stash before cherrypick of "readme format"

---
+++ Infrastructure/SignalR/SignalRWorks.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace mvccoresb.Infrastructure.SignalR
+{
+    using chat.Domain.APImodels;
+
+    public class SignalRWorks : Hub
+    {
+        public async Task StartWork(){
+            await Clients.All.SendAsync("WorkStatus",new WorkQueued());
+            await BackGroundWork.FakeLongRunningTask(2000);
+            await Clients.All.SendAsync("WorkStatus", new WorkStarted());
+            await BackGroundWork.FakeLongRunningTask(3000);
+            await Clients.All.SendAsync("WorkStatus", new WorkFinished());       
+        }
+    }
+    
+    public class BackGroundWork
+    {       
+        public static async Task FakeLongRunningTask()
+        {
+            await Task.Factory.StartNew(() => Thread.Sleep(5000));
+        }
+
+        public static async Task FakeLongRunningTask(int ms)
+        {
+            await Task.Factory.StartNew(() => Thread.Sleep(ms));
+        }
+    }
+
+}
