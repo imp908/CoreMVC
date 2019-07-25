7fbc4603eec2be0d931ef38cb5904b2f7bb98f85
imp908 <imp908@yandex.ru>
2019-07-25 23:48:42 +0300

    newOrder currencies manager API resp

--- crmvcsb/API/Areas/TestArea/FolderControllers/NewOrderController.cs
+++ crmvcsb/API/Areas/TestArea/FolderControllers/NewOrderController.cs
@@ -1,4 +1,9 @@
+
+using System;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
 using Newtonsoft.Json;
 
 using crmvcsb.Infrastructure.EF;
@@ -7,22 +12,26 @@ using crmvcsb.Domain.Interfaces;
 using AutoMapper;
 using crmvcsb.Domain.NewOrder.API;
 
+using crmvcsb.Domain.NewOrder;
+
+
 namespace crmvcsb.API.Areas.TestArea.FolderControllers
 {
     [Area("NewOrder")]
     [Route("Currency")]
     public class NewOrderController
     {
-     
 
-        public NewOrderController() {
+        private INewOrdermanager _manager;
 
+        public NewOrderController(INewOrdermanager manager) {
+            _manager = manager;
         }
 
         [HttpGet("Get")]
-        public IActionResult GetCurrency(){
+        public async ActionResult<Task<IList<ICrossCurrenciesAPI>>> GetCurrency(GetCurrencyCommand command){
             try{
-
+                return await _manager.GetCurrencyCrossRates(command);
             }catch(Exception e){
                 
             }
--- crmvcsb/Infrastructure/EF/newOrder/NewOrdermanager.cs
+++ crmvcsb/Infrastructure/EF/newOrder/NewOrdermanager.cs
@@ -1,19 +1,19 @@
+
 namespace crmvcsb.Infrastructure.EF.newOrder
 {
     using System.Linq;
-    using System.Linq.Expressions;
-    using crmvcsb.Infrastructure.EF;
-    using crmvcsb.Infrastructure.EF.newOrder;
-    using crmvcsb.Domain.NewOrder.DAL;
-    using crmvcsb.Domain.NewOrder.API;
-    using crmvcsb.Domain.Interfaces;
+
     using AutoMapper;
     using Microsoft.EntityFrameworkCore;
     using System.Threading.Tasks;
-    using System.Threading;
     using System.Collections.Generic;
 
-    public class NewOrdermanager
+    using crmvcsb.Domain.NewOrder.DAL;
+    using crmvcsb.Domain.NewOrder.API;
+    using crmvcsb.Domain.Interfaces;
+    using crmvcsb.Domain.NewOrder;
+
+    public class NewOrdermanager : INewOrdermanager
     {
         IRepository _repository;
         IMapper _mapper;
@@ -24,16 +24,15 @@ namespace crmvcsb.Infrastructure.EF.newOrder
             _mapper = mapper;
         }
 
-        public async Task<IList<CrossCurrenciesAPI>> GetCurrencyCrossRates(string isoCode) {
-            
-            var result = await _repository.QueryByFilter<CurrencyRatesDAL>(c => c.CurrencyFrom.IsoCode.ToLower() == isoCode.ToLower())
-            .Select(c => new CrossCurrenciesAPI () {From = c.CurrencyFrom.IsoCode, To = c.CurrencyTo.IsoCode, Rate = c.Rate }).ToListAsync();
+        public async Task<IList<ICrossCurrenciesAPI>> GetCurrencyCrossRates(GetCurrencyCommand command) {
 
-            if(result?.Any() == true) {
-                return result;
-            }
-            
-            return some code
+            List<CurrencyRatesDAL> r = await _repository
+                .QueryByFilter<CurrencyRatesDAL>(c => c.CurrencyFrom.IsoCode.ToLower() == command.IsoCode.ToLower())
+                .ToListAsync();
+
+            var result = _mapper.Map<IList<CurrencyRatesDAL>, IList<CrossCurrenciesAPI>>(r);
+
+            return result.Cast<ICrossCurrenciesAPI>().ToList();
         }
     }
 }
