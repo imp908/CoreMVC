a1b478d28115f78ab2eaaa836472dedcbff9be75
Unknown <imp908@yandex.ru>
2020-05-02 17:08:56 +0300

    BLGN rep

--- crmvcsb/Startup.cs
+++ crmvcsb/Startup.cs
@@ -30,14 +30,14 @@ namespace crmvcsb
     using crmvcsb.Domain.Universal.IRepository;
 
 
-    using crmvcsb.Domain.DomainSpecific.NewOrder;
     using crmvcsb.Domain.DomainSpecific.Blogging.API;
     using crmvcsb.Domain.DomainSpecific.Blogging.BLL;
-    using crmvcsb.Domain.DomainSpecific.Blogging.DAL;
     using crmvcsb.Infrastructure.EF.Blogging;
+    using crmvcsb.Infrastructure.EF.Currencies;
+    using crmvcsb.Domain.DomainSpecific.Currency.DAL;
+    using crmvcsb.Domain.DomainSpecific.Currency.API;
 
- 
-   
+
     using crmvcsb.Domain.Universal;
 
     /*Build in logging*/
