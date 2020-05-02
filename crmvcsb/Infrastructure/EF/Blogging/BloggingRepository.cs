5ad159e37c64f37750eb58129db2cdb287b163a3
imp908 <imp908@yandex.ru>
2020-05-02 17:01:52 +0300

    BLGN2 rep

---
+++ crmvcsb/Infrastructure/EF/Blogging/BloggingRepository.cs
@@ -0,0 +1,20 @@
+﻿
+namespace crmvcsb.Infrastructure.EF.Blogging
+{
+    using AutoMapper;
+  
+    using Microsoft.EntityFrameworkCore;
+
+    using crmvcsb.Domain.Universal.IRepository;
+    using crmvcsb.Infrastructure.EF;
+
+    public class BloggingRepository : RepositoryEF, IRepositoryEF, IRepository
+    {
+        internal IMapper _mapper { get; set; }
+        internal IRepositoryEF _repository { get; set; }
+        public BloggingRepository(DbContext context, IRepositoryEF repository, IMapper mapper) : base(context)
+        {
+
+        }
+    }
+}
