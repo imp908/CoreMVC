b3d8f7396cfcd723b31c2f5ef2c57c9475d98622
imp908 <imp908@yandex.ru>
2020-11-04 14:14:30 +0300

    db checker eddit

--- crmvcsb/Infrastructure/Checkers/CheckersOneFile.cs
+++ crmvcsb/Infrastructure/Checkers/CheckersOneFile.cs
@@ -11,6 +11,10 @@ namespace InfrastructureCheckers
 
     using AutoMapper;
 
+    using crmvcsb.Infrastructure.EF.Currencies;
+    using crmvcsb.Domain.DomainSpecific.Currency.DAL;
+
+    using crmvcsb.Domain.DomainSpecific.Currency;
 
     public static class RepoAndUOWCheck
     {
@@ -18,12 +22,56 @@ namespace InfrastructureCheckers
         //static string connectionStringSQL = "Server=HP-HP000114\\SQLEXPRESS02;Database=EFdb;Trusted_Connection=True;";
         static string connectionStringSQL = "Server=AAAPC;Database=testdb;User Id=tl;Password=QwErT123;";
 
-        public static void GO(){
-           
+        public static void GO()
+        {
+            DbWithRepoReinitCheck();
         }
-        
-       
-      
+
+        public static void DbWithRepoReinitCheck()
+        {
+
+            using (CurrencyContext context = new CurrencyContext(
+                new DbContextOptionsBuilder<CurrencyContext>()
+                    .UseSqlServer(connectionStringSQL).Options))
+            {
+                crmvcsb.Infrastructure.EF.RepositoryEF repo = new crmvcsb.Infrastructure.EF.RepositoryEF(context);
+
+                List<CurrencyDAL> currencies = repo.QueryByFilter<CurrencyDAL>(s => s.Id != 0).ToList();
+                repo.DeleteRange(currencies);
+                repo.Save();
+
+                CurrencyService currencyService = new CurrencyService(repo);
+                currencyService.ReInitialize();
+
+                repo.Add(new CurrencyDAL() { Id = 1, Name = "KRW", IsoCode = "KRW" });
+  
+                try
+                {
+                    repo.SaveIdentity("Blogs");
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+
+                repo.AddRange(new List<CurrencyDAL>() {
+                    new CurrencyDAL() { Id = 1, Name = "NOK", IsoCode = "NOK" }
+                    ,new CurrencyDAL() { Id = 1, Name = "TRY", IsoCode = "TRY" }
+                });
+
+                try
+                {
+                    repo.SaveIdentity("Posts");
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+
+            }
+        }
+
+
     }
 }
 
